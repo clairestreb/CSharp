77fc71cbdcb15ca521dff4db2c3fabf8e92ec312
clairestreb <clairestreb@hotmail.com>
2014-08-08 10:15:56 -0400

    original checkin

---
+++ _SolutionFiles/_Template.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace ?.TODO
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class TODO
+    {
+        #region Member variables
+        #endregion Member variables
+
+        #region Construct Destruct
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public TODO(string TODO)
+        {
+        }
+
+        #endregion Construct Destruct
+
+        #region Events
+        #endregion Events
+
+        #region Public Properties
+        #endregion Public Properties
+
+        #region Public Methods
+        #endregion Public Methods
+
+        #region Protected Methods/Properties
+        #endregion Protected Methods/Properties
+
+        #region Private Methods
+        #endregion Private Methods
+
+        #region Private Properties
+        #endregion Private Properties
+
+        #region Private Classes
+        #endregion Private Classes
+
+    } // end class
+
+    #region Public Classes
+    #endregion Public Classes
+
+} // end namespace
